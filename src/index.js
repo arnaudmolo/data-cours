5fe20132f392fb264428de6ee3af74b8f6a5f229
Arnaud Pichon <arnaud@onesime.fr>
2018-03-16 18:08:42 +0100

    Add todos and screenshot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './styles.css'
-import { csv } from 'd3'
 
-csv('/public/basic.csv', data => {
-  console.log(data)
+const todosList = document.querySelector('#todos-container')
+const input = document.querySelector('#input')
+
+const renderATodo = (todo) => `
+<li class="list-element">
+  <p class="todo-label">${todo.label}</p>
+  <button class="button remove-button">x</button>
+</li>
+`
+const createTodo = (string) => {
+  return {
+    label: string,
+    completed: false,
+    createdAt: new Date()
+  }
+}
+input.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    todosList.innerText = renderATodo(createTodo(event.target.value))
+    event.target.value = ""
+  }
 })
