19729c7913561956bc5fe9c882358e3fa5de0e3f
Arnaud Pichon <arnaud@onesime.fr>
2018-03-27 15:48:14 +0200

    all working together

--- src/View.js
+++ src/View.js
@@ -3,13 +3,6 @@ import * as d3 from 'd3'
 
 const outerWidth = 800
 const outerHeight = 500
-const radius = 2
-const margins = {
-  top: 20,
-  bottom: 20,
-  left: 20,
-  right: 20
-}
 // Popup container. Styled to follow the svg.
 const $popupContainer = document.querySelector('#content')
 const format = d3.formatPrefix(`.${d3.precisionPrefix(1e5, 1.3e6)}`, 3e6)
@@ -21,14 +14,7 @@ const translate = (x, y) => `translate(${x}px, ${y}px)`
 const svg = d3.select('body').append('svg')
   .attr('width',  outerWidth)
   .attr('height', outerHeight)
-
-// Scales.
-const xScale = d3.scaleLinear()
-  .range([margins.left, outerWidth - margins.right])
-const yScale = d3.scaleLinear()
-  .range([outerHeight - margins.top, margins.bottom])
-const rScale = d3.scaleSqrt()
-const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+const circlesGroup = svg.append('g')
 
 // Views.
 // function creating a snipped line.
@@ -66,47 +52,27 @@ const createPopup = (x, y, container) => (data) => {
 }
 
 // function that render the visualisation.
-// argument 1 (xCol): function that define the x of each data.
-// argument 2 (yCol): function that define the y of each data.
-// argument 3 (radiusCol): function that define the radius of each data.
-// argument 4 (peoplePerPixel): function that define the people pet pixel ratio.
-// argument 5 (data): data to build the visualisation
+// argument 1 (x): function that define the x position of each data.
+// argument 2 (y): function that define the y position of each data.
+// argument 3 (r): function that define the radius of each data.
+// argument 4 (data): data to build the visualisation
 // return : a function taking 1 argument (data) to build the popup.
-export function render(xCol, yCol, radiusCol, peoplePerPixel, data) {
-  // Composition is cool. Seel http://ramdajs.com/docs/#compose
-  // Equivalent to : d => xScale(xCol(d))
-  const x = R.compose(xScale, xCol)
-  const y = R.compose(yScale, yCol)
-  const r = R.compose(rScale, radiusCol)
-  const fill = R.compose(colorScale, radiusCol)
-  const radiusDataExtent = d3.extent(data, radiusCol)
-
-  xScale.domain(d3.extent(data, xCol))
-  yScale.domain(d3.extent(data, yCol))
-  // Need to fix the rScale domain on a fix point.
-  // We don't want the representation to change depending on data.
-  rScale.domain(radiusDataExtent).range([
-    0, Math.sqrt(radiusDataExtent[1] / (peoplePerPixel * Math.PI))
-  ])
-  colorScale.domain(radiusDataExtent)
-
-  const circlesGroup = svg.append('g')
-  const circles = circlesGroup.selectAll('circle').data(data)
-
-  // D3.zoom plugin: https://github.com/d3/d3-zoom.
-  // Detect all zoom events.
-  svg.call(d3.zoom().on('zoom', _ =>
-    // apply zoom transform to all circles via the containing group.
-    circlesGroup.attr('transform', d3.event.transform)
-  ))
+export function render(x, y, r, data) {
+  const circles = circlesGroup.selectAll('circle').data(data, r)
+  circles
+    .exit()
+    .transition()
+    .duration(2000)
+    .attr('r', 0)
+    .remove()
 
   circles
     .enter()
     .append('circle')
+    .attr('id', d => d.label)
     .attr('cx', x)
     .attr('cy', y)
-    .attr('r', r)
-    .attr('fill', fill)
+    .attr('r', 0)
     .on('mouseenter', function (d) {
       // Apply a notable interaction.
       d3.select(this)
@@ -129,11 +95,12 @@ export function render(xCol, yCol, radiusCol, peoplePerPixel, data) {
       d3.select(this).transition().duration(100).attr('r', r)
       return createPopup(x, y, d3.select($popupContainer))([])
     })
-
-  circles
-    .exit()
     .transition()
     .duration(2000)
-    .attr('r', 0)
-    .remove()
+    .attr('r', r)
+
+  circles
+    .transition()
+    .duration(2000)
+    .attr('r', r)
 }
--- src/index.js
+++ src/index.js
@@ -22,39 +22,84 @@ d3.json('https://unpkg.com/d3-format@1/locale/fr-FR.json', function(error, local
     d3.formatDefaultLocale(locale)
 })
 
+const reducer = (data) => {
+  let result = data
+  if ($townFilter.value.length) {
+    const val = $townFilter.value
+    result = result.filter(d => d.label.slice(0, val.length) === val)
+  }
+  if ($levelFilter.value) {
+    const val = $levelFilter.value
+    result = result.filter(d => d.population > val)
+  }
+  return result
+}
+
 // Download the needed data
 d3.csv('public/countries_population.csv', type, data => {
   // set pixel ratio default value
   $pixelRatio.value = 100000
+  $levelFilter.value = 0
+  $townFilter.value = ''
+
+  const outerWidth = 800
+  const outerHeight = 500
+  const radius = 2
+  const margins = {
+    top: 20,
+    bottom: 20,
+    left: 20,
+    right: 20
+  }
+
+  // Scales.
+  const xScale = d3.scaleLinear()
+    .range([margins.left, outerWidth - margins.right])
+  const yScale = d3.scaleLinear()
+    .range([outerHeight - margins.top, margins.bottom])
+  const rScale = d3.scaleSqrt()
+
+  // Create accessors
+  // http://ramdajs.com/docs/#prop
+  // Equivalent : d => d['longitude']
+  const xCol = R.prop('longitude')
+  const yCol = R.prop('latitude')
+  const rCol = R.prop('population')
+
+  const radiusDataExtent = d3.extent(data, rCol)
+
+  xScale.domain(d3.extent(data, xCol))
+  yScale.domain(d3.extent(data, yCol))
+  // Need to fix the rScale domain on a fix point.
+  // We don't want the representation to change depending on data.
+  rScale.domain(radiusDataExtent).range([
+    0, Math.sqrt(radiusDataExtent[1] / ($pixelRatio.value * Math.PI))
+  ])
+
   // Currying the render function to avoid repetition:
   // http://ramdajs.com/docs/#curry
   // Equivalent : (x, y, z) => (peoplePerPixel, data)
   const toRender = R.curry(render)(
-    // Create accessors
-    // http://ramdajs.com/docs/#prop
-    // Equivalent : d => d['longitude']
-    R.prop('longitude'),
-    R.prop('latitude'),
-    R.prop('population')
+    // Composition is cool. Seel http://ramdajs.com/docs/#compose
+    // Equivalent to : d => xScale(xCol(d))
+    R.compose(xScale, xCol),
+    R.compose(yScale, yCol),
+    R.compose(rScale, rCol)
   )
-  toRender(
-    $pixelRatio.value,
-    data
-  )
-  $pixelRatio.addEventListener('change', (event) => {
-    toRender(
-      $pixelRatio.value,
-      data
-    )
+  toRender(reducer(data))
+  $pixelRatio.addEventListener('change', _ => {
+    if ($pixelRatio.value.length === 0) {
+      $pixelRatio.value = 100000
+    }
+    rScale.domain(radiusDataExtent).range([
+      0, Math.sqrt(radiusDataExtent[1] / ($pixelRatio.value * Math.PI))
+    ])
+    toRender(reducer(data))
   })
-  $levelFilter.addEventListener('change', event => toRender(
-    $pixelRatio.value,
-    data.filter(d => d.population > event.target.value)
+  $levelFilter.addEventListener('change', _ => toRender(
+    reducer(data)
   ))
-  $townFilter.addEventListener('change', event => toRender(
-    $pixelRatio.value,
-    data.filter(d =>
-      d.label.slice(0, event.target.value.length) === event.target.value
-    )
+  $townFilter.addEventListener('change', _ => toRender(
+    reducer(data)
   ))
 })
