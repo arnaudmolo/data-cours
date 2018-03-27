ccc6ea6fb1a828081291557062e02345ccdfde59
Arnaud Pichon <arnaud@onesime.fr>
2018-03-27 14:33:22 +0200

    comments

--- src/View.js
+++ src/View.js
@@ -10,19 +10,28 @@ const margins = {
   left: 20,
   right: 20
 }
+// Popup container. Styled to follow the svg.
 const $popupContainer = document.querySelector('#content')
+const format = d3.formatPrefix(`.${d3.precisionPrefix(1e5, 1.3e6)}`, 3e6)
 
+// Snippet.
 const translate = (x, y) => `translate(${x}px, ${y}px)`
 
+// Visualisation canvas.
 const svg = d3.select('body').append('svg')
   .attr('width',  outerWidth)
   .attr('height', outerHeight)
+
+// Scales.
 const xScale = d3.scaleLinear()
   .range([margins.left, outerWidth - margins.right])
 const yScale = d3.scaleLinear()
   .range([outerHeight - margins.top, margins.bottom])
 const rScale = d3.scaleSqrt()
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+
+// Views.
+// function creating a snipped line.
 const createText = text => selection =>
   selection
     .append('p')
@@ -31,7 +40,12 @@ const createText = text => selection =>
     .transition()
     .attr('opacity', 1)
 
-const renderPopup = (x, y, container) => (data) => {
+// function creating the Popup.
+// argument 1 (x): function that define the x position.
+// argument 2 (y): function that define the y position.
+// argument 3 (container): dom element to append the element.
+// return : a function taking 1 argument (data) to build the popup.
+const createPopup = (x, y, container) => (data) => {
   const bar = container.selectAll('div').data(data)
   bar
     .enter()
@@ -39,17 +53,28 @@ const renderPopup = (x, y, container) => (data) => {
     .attr('class', 'popup-container')
     .style('transform', d => translate(x(d), y(d)))
     .each(function (d) {
-      const innerBar = d3.select(this)
-      innerBar.call(createText(d => d.label))
-      const format = d3.formatPrefix(`.${d3.precisionPrefix(1e5, 1.3e6)}`, 3e6)
-      innerBar.call(createText(d => format(d.population)))
+      // transform a DOM element to d3 selection.
+      const el = d3.select(this)
+      // Syntax to call a function on a d3 selection.
+      el
+        .call(createText(d => d.label))
+        .call(createText(d => format(d.population)))
     })
     bar
       .exit()
       .remove()
 }
 
+// function that render the visualisation.
+// argument 1 (xCol): function that define the x of each data.
+// argument 2 (yCol): function that define the y of each data.
+// argument 3 (radiusCol): function that define the radius of each data.
+// argument 4 (peoplePerPixel): function that define the people pet pixel ratio.
+// argument 5 (data): data to build the visualisation
+// return : a function taking 1 argument (data) to build the popup.
 export function render(xCol, yCol, radiusCol, peoplePerPixel, data) {
+  // Composition is cool. Seel http://ramdajs.com/docs/#compose
+  // Equivalent to : d => xScale(xCol(d))
   const x = R.compose(xScale, xCol)
   const y = R.compose(yScale, yCol)
   const r = R.compose(rScale, radiusCol)
@@ -58,15 +83,20 @@ export function render(xCol, yCol, radiusCol, peoplePerPixel, data) {
 
   xScale.domain(d3.extent(data, xCol))
   yScale.domain(d3.extent(data, yCol))
+  // Need to fix the rScale domain on a fix point.
+  // We don't want the representation to change depending on data.
   rScale.domain(radiusDataExtent).range([
     0, Math.sqrt(radiusDataExtent[1] / (peoplePerPixel * Math.PI))
   ])
   colorScale.domain(radiusDataExtent)
+
   const circlesGroup = svg.append('g')
-  const markG = svg.append('g')
   const circles = circlesGroup.selectAll('circle').data(data)
 
+  // D3.zoom plugin: https://github.com/d3/d3-zoom.
+  // Detect all zoom events.
   svg.call(d3.zoom().on('zoom', _ =>
+    // apply zoom transform to all circles via the containing group.
     circlesGroup.attr('transform', d3.event.transform)
   ))
 
@@ -78,23 +108,26 @@ export function render(xCol, yCol, radiusCol, peoplePerPixel, data) {
     .attr('r', r)
     .attr('fill', fill)
     .on('mouseenter', function (d) {
-      const el = d3.select(this)
-      el.transition()
+      // Apply a notable interaction.
+      d3.select(this)
+        .transition()
         .duration(100)
         .attr('r', d => r(d) * 2)
+      // Finding the node position on the page to locate the popup.
       const bbox = svg.node().getBoundingClientRect()
       const doc = document.documentElement
       const left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0)
       const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0)
-      renderPopup(
+      createPopup(
         _ => d3.event.pageX - bbox.x - left,
         _ => d3.event.pageY - bbox.y - top,
         d3.select($popupContainer)
       )([d])
     })
     .on('mouseleave', function (d) {
+      // Set the normal size and remove the popup
       d3.select(this).transition().duration(100).attr('r', r)
-      return renderPopup(x, y, d3.select($popupContainer))([])
+      return createPopup(x, y, d3.select($popupContainer))([])
     })
 
   circles
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as R from 'ramda'
 import { transform as d3transform } from 'd3-transform'
 
 import { render } from './View'
+
 const $pixelRatio = document.querySelector('#ppx')
 const $levelFilter = document.querySelector('#filter')
 const $townFilter = document.querySelector('#town')
@@ -14,13 +15,24 @@ const type = d => ({
   longitude: +d.longitude,
   label: d.name
 })
-d3.json("https://unpkg.com/d3-format@1/locale/fr-FR.json", function(error, locale) {
-if (error) throw error
-  d3.formatDefaultLocale(locale)
+
+d3.json('https://unpkg.com/d3-format@1/locale/fr-FR.json', function(error, locale) {
+  // Download french formats
+  if (error) throw error
+    d3.formatDefaultLocale(locale)
 })
+
+// Download the needed data
 d3.csv('public/countries_population.csv', type, data => {
+  // set pixel ratio default value
   $pixelRatio.value = 100000
+  // Currying the render function to avoid repetition:
+  // http://ramdajs.com/docs/#curry
+  // Equivalent : (x, y, z) => (peoplePerPixel, data)
   const toRender = R.curry(render)(
+    // Create accessors
+    // http://ramdajs.com/docs/#prop
+    // Equivalent : d => d['longitude']
     R.prop('longitude'),
     R.prop('latitude'),
     R.prop('population')
