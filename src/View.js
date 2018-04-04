e34dc146af7d9517fc8022d28f011e7c910ce8c5
Arnaud Pichon <arnaud@onesime.fr>
2018-04-04 17:02:16 +0200

    bon brush

--- src/View.js
+++ src/View.js
@@ -1,128 +1,68 @@
 import * as d3 from 'd3'
-import * as R from 'ramda'
 
-const outerWidth = 800
-const outerHeight = 500
-// Popup container. Styled to follow the svg.
-const $popupContainer = document.querySelector('#content')
-const format = d3.formatPrefix(`.${d3.precisionPrefix(1e5, 1.3e6)}`, 3e6)
+// const format = d3.formatPrefix(`.${d3.precisionPrefix(1e5, 1.3e6)}`, 3e6)
 
 // Snippet.
-const translate = (x, y) => `translate(${x}px, ${y}px)`
+// const translate = (x, y) => `translate(${x}px, ${y}px)`
+const monthLegend = document.getElementById('month')
+const width = 300
+const height = 10
 
-// Views.
-// function creating a snipped line.
-const createText = text => selection =>
+const brushScale = d3.scaleLinear().domain([0, 300]).range([0, 11]).interpolate(d3.interpolateRound)
+
+function createBrush (selection, map) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+  function filterBy ([minMonth, maxMonth]) {
+    const filters = [
+      'all',
+      ['>=', 'month', minMonth],
+      ['<=', 'month', maxMonth]
+    ]
+    map.setFilter('points', filters)
+    map.setFilter('route', filters)
+    monthLegend.innerText = `Timeline from ${months[minMonth]} to ${months[maxMonth]}`
+  }
+  function brushed (e) {
+    filterBy(d3.event.selection.map(brushScale))
+  }
+  filterBy([0, 11])
+
   selection
-    .append('p')
-    .text(text)
-    .attr('opacity', 0)
-    .transition()
-    .attr('opacity', 1)
-
-// function creating the Popup.
-// argument 1 (x): function that define the x position.
-// argument 2 (y): function that define the y position.
-// argument 3 (container): dom element to append the element.
-// return : a function taking 1 argument (data) to build the popup.
-const createPopup = (x, y, container) => (data) => {
-  const bar = container.selectAll('div').data(data)
-  bar
+    .append('g')
+    .attr('class', 'axis')
+    .selectAll('text')
+    .data(months)
     .enter()
-    .append('div')
-    .attr('class', 'popup-container')
-    .style('transform', d => translate(x(d), y(d)))
-    .each(function (d) {
-      // transform a DOM element to d3 selection.
-      const el = d3.select(this)
-      // Syntax to call a function on a d3 selection.
-      el
-        .call(createText(d => d.label))
-        .call(createText(d => format(d.population)))
-    })
-    bar
-      .exit()
-      .remove()
+    .append('text')
+    .text(e => e.slice(0, 7))
+    .attr('x', (_, i) => brushScale.invert(i))
+    .attr('y', 15)
+
+  selection
+    .append('g')
+    .attr('class', 'brush')
+    .call(d3.brushX().on('brush', brushed))
 }
 
-// function that render the visualisation.
-// argument 1 (x): function that define the x position of each data.
-// argument 2 (y): function that define the y position of each data.
-// argument 3 (r): function that define the radius of each data.
-// argument 4 (data): data to build the visualisation.
-// return : circles d3 selection.
-export function render(geo, r, features) {
-  const x = d => geo([d.longitude, d.latitude])[0]
-  const y = d => geo([d.longitude, d.latitude])[1]
-
+export function render (map) {
   // Visualisation canvas.
-  const svg = d3.select('body').append('svg')
-    .attr('width',  outerWidth)
-    .attr('height', outerHeight)
-
-  const circlesGroup = svg.append('g')
-  const mapPath = circlesGroup.append('path').attr('class', 'map')
-  const geoPath = d3.geoPath(geo)
-  mapPath
-    .datum(features)
-    .attr('d', geoPath)
-    .on('click', d => {
-      console.log(d)
-    })
-
+  const svg = d3.select('#context')
+  const $brush = svg.append('g')
+  $brush.call(createBrush, map)
   return (data) => {
-    mapPath
-      .datum(features)
-      .attr('d', geoPath)
-
-    const fill = R.compose(
-      d3
-        .scaleOrdinal(d3.schemeCategory10)
-        .domain(d3.extent(data, r)),
-      d => d.latLng
-    )
-
-    const circles = circlesGroup.selectAll('circle').data(data)
-
-    circles
-      .enter()
-      .append('circle')
-      .attr('fill', fill)
-      .attr('cx', x)
-      .attr('cy', y)
-      .attr('r', r)
-      .on('mouseenter', function (d) {
-        // Apply a notable interaction.
-        d3.select(this)
-          .transition()
-          .duration(100)
-          .attr('r', d => r(d) * 2)
-        // Finding the node position on the page to locate the popup.
-        const bbox = svg.node().getBoundingClientRect()
-        const doc = document.documentElement
-        const left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0)
-        const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0)
-        createPopup(
-          _ => d3.event.pageX - bbox.x - left,
-          _ => d3.event.pageY - bbox.y - top,
-          d3.select($popupContainer)
-        )([d])
-      })
-      .on('mouseleave', function (d) {
-        // Set the normal size and remove the popup
-        d3.select(this).transition().duration(100).attr('r', r)
-        return createPopup(x, y, d3.select($popupContainer))([])
-      })
-
-    circles
-      .exit()
-      .transition()
-      .duration(2000)
-      .attr('r', 0)
-      .remove()
-
-    circles
-      .attr('cx', x)
-      .attr('cy', y)
+    // x.domain(d3.extent(data, d => d.createdAt))
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import './styles.css'
-import * as d3 from 'd3'
+import mapboxgl from 'mapbox-gl'
+import moment from 'moment'
+import { render } from './View'
 
-const type = d => ({
-  population: parseInt(d.population),
-  latitude: parseFloat(d.latitude),
-  longitude: +d.longitude,
-  label: d.name
-})
-
-d3.json('https://unpkg.com/d3-format@1/locale/fr-FR.json', function (error, locale) {
-  // Download french formats.
-  // Here to show syntax example. Don't do it.
-  if (error) throw error
-  d3.formatDefaultLocale(locale)
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJuYXVkbW9sbyIsImEiOiJjaW5zbjgxYXQwMGowdzdrbGQ5a2NlaGpuIn0.JxCzxWoDULTqfKatKDFg9g'
+const map = new mapboxgl.Map({
+  container: 'content',
+  style: 'mapbox://styles/arnaudmolo/cjfk1zs7bejmr2rnypmmdsy4s',
+  center: [0.380435, 47.530053],
+  zoom: 5.85,
+  bearing: 0,
+  pitch: 0
 })
 
 // Return different version of the data
@@ -21,12 +19,104 @@ const reducer = (state) => {
   return state
 }
 
+const createPopup = properties => `<div>
+  <p>${properties.city}</p>
+  <p>${moment(properties.createdAt).format('dddd, MMMM Do YYYY, H:mm:ss')}
+</div>`
+
 const startup = async () => {
-  return d3.csvParse(
-    await (
-      await window.fetch('public/countries_population.csv')
-    ).text()
-  ).map(type).filter(e => e)
+  let data = await (
+    await window.fetch('public/geolocs.geojson')
+  ).json()
+
+  data = {
+    ...data,
+    features: data.features.map(d => {
+      const createdAt = moment(d.properties.createdAt)
+      const month = createdAt.month()
+      return {
+        ...d,
+        properties: {
+          ...d.properties,
+          createdAt,
+          month
+        }
+      }
+    })
+  }
+
+  const lines = {
+    type: 'FeatureCollection',
+    features: data.features.map((d, i) => {
+      const next = data.features[i + 1] ? i + 1 : 0
+      return {
+        ...d,
+        type: 'Feature',
+        geometry: {
+          'type': 'LineString',
+          coordinates: [
+            [
+              data.features[i].geometry.coordinates[0],
+              data.features[i].geometry.coordinates[1]
+            ],
+            [
+              data.features[next].geometry.coordinates[0],
+              data.features[next].geometry.coordinates[1]
+            ]
+          ]
+        }
+      }
+    })
+  }
+
+  map.on('load', () => {
+    map.addSource('facebook', {type: 'geojson', data})
+    map.addSource('facebook-lines', {
+      'type': 'geojson',
+      'data': lines
+    })
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'facebook-lines',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      'paint': {
+        'line-color': 'hsl(154, 44%, 47%)',
+        'line-width': 2
+      }
+    })
+
+    map.addLayer({
+      id: 'points',
+      type: 'circle',
+      source: 'facebook',
+      paint: {
+        'circle-color': 'red'
+      }
+    })
+
+    render(map)(data)
+
+    map.on('click', 'points', (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice()
+      const description = e.features[0].properties
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(createPopup(description))
+        .addTo(map)
+    })
+  })
 }
 
 startup()
