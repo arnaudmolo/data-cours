b418eaacc79c963e5a2669e15c107e6b9eb456e1
serge_nanou <serge.nanou@hetic.net>
2018-04-27 23:46:20 +0200

    Simulation de l'attaque des Zombies aux USA avec la vulnerabilité en fonction de la forte  coloration et les populations touchées réprésenté par les cercles

--- src/index.js
+++ src/index.js
@@ -1,58 +1,172 @@
 import './styles.css'
 import * as d3 from 'd3'
-import moment from 'moment'
 
-import { render } from './View'
-import renderBrush from './Brush'
 
-const type = d => {
-  return {
-    ...d,
-    properties: {
-      ...d.properties,
-      date: moment(d.properties.createdAt)
+
+
+// Width and height
+var chart_width     =   800;
+var chart_height    =   600;
+var color           =   d3.scaleQuantize().range([
+    'rgb(255,255,229)','rgb(247,252,185)','rgb(217,240,163)',
+    'rgb(173,221,142)','rgb(120,198,121)','rgb(65,171,93)',
+    'rgb(35,132,67)','rgb(0,104,55)','rgb(0,69,41)'
+]);
+
+// Projection
+var projection      =   d3.geoAlbersUsa()
+    .translate([ 0,0 ]);
+var path            =   d3.geoPath( projection );
+    // .projection( projection );
+
+// Create SVG
+var svg             =   d3.select("#chart")
+    .append("svg")
+    .attr("width", chart_width)
+    .attr("height", chart_height);
+
+var zoom_map        =   d3.zoom()
+    .scaleExtent([ 0.5, 3.0 ])
+    .translateExtent([
+        [ -1000, -500 ],
+        [ 1000, 500 ]
+    ])
+    .on( 'zoom', function(){
+    // console.log( d3.event );
+    var offset      =   [
+        d3.event.transform.x,
+        d3.event.transform.y
+    ];
+    var scale       =   d3.event.transform.k * 2000;
+
+    projection.translate( offset )
+        .scale( scale );
+
+    svg.selectAll( 'path' )
+        .transition()
+        .attr( 'd', path );
+
+    svg.selectAll( 'circle' )
+        .transition()
+        .attr( "cx", function(d) {
+            return projection([d.lon, d.lat])[0];
+        })
+        .attr( "cy", function(d) {
+            return projection([d.lon, d.lat])[1];
+        });
+});
+
+var map             =   svg.append( 'g' )
+    .attr( 'id', 'map' )
+    .call( zoom_map )
+    .call(
+        zoom_map.transform,
+        d3.zoomIdentity
+            .translate( chart_width / 2, chart_height / 2 )
+            .scale( 1 )
+    );
+
+map.append( 'rect' )
+    .attr( 'x', 0 )
+    .attr( 'y', 0 )
+    .attr( 'width', chart_width )
+    .attr( 'height', chart_height )
+    .attr( 'opacity', 0 );
+
+// Data
+d3.json( 'public/zombie-attacks.json', function( zombie_data ){
+    color.domain([
+        d3.min( zombie_data, function(d){
+            return d.num;
+        }),
+        d3.max( zombie_data, function(d){
+            return d.num;
+        })
+    ]);
+
+    d3.json( 'public/us.json', function( us_data ){
+        us_data.features.forEach(function(us_e, us_i){
+            zombie_data.forEach(function(z_e,z_i){
+                if( us_e.properties.name !== z_e.state ){
+                    return null;
+                }
+
+                us_data.features[us_i].properties.num   =   parseFloat(z_e.num);
+            });
+        });
+
+        // console.log(us_data);
+
+        map.selectAll( 'path' )
+            .data( us_data.features )
+            .enter()
+            .append( 'path' )
+            .attr( 'd', path )
+            .attr( 'fill', function( d ){
+                var num         =   d.properties.num;
+                return num ? color( num ) : '#ddd';
+            })
+            .attr( 'stroke', '#fff' )
+            .attr( 'stroke-width', 1 );
+
+        draw_cities();
+    });
+});
+
+function draw_cities(){
+    d3.json( 'public/us-cities.json', function( city_data ){
+        map.selectAll("circle")
+            .data(city_data)
+            .enter()
+            .append( "circle" )
+            .style( "fill", "#9D497A" )
+            .style( "opacity", 0.8 )
+            .attr( 'cx', function( d ){
+                return projection([ d.lon, d.lat ])[0];
+            })
+            .attr( 'cy', function( d ){
+                return projection([ d.lon, d.lat ])[1];
+            })
+            .attr( 'r', function(d){
+                return Math.sqrt( parseInt(d.population) * 0.00005 );
+            })
+            .append( 'title' )
+            .text(function(d){
+                return d.city;
+            });
+    });
+}
+
+d3.selectAll( '#buttons button.panning' ).on( 'click', function(){
+    var x           =   0;
+    var y           =   0;
+    var distance    =   100;
+    var direction   =   d3.select( this ).attr( 'class' ).replace( 'panning ', '' );
+
+    if( direction === "up" ){
+        y           +=  distance; // Increase y offset
+    }else if( direction === "down" ){
+        y           -=  distance; // Decrease y offset
+    }else if( direction === "left" ){
+        x           +=  distance; // Increase x offset
+    }else if( direction === "right" ){
+        x           -=  distance; // Decrease x offset
     }
-  }
-}
 
-d3.json('https://unpkg.com/d3-format@1/locale/fr-FR.json', function (error, locale) {
-  // Download french formats.
-  // Here to show syntax example. Don't do it.
-  if (error) throw error
-  d3.formatDefaultLocale(locale)
-})
+    map.transition()
+        .call( zoom_map.translateBy, x, y );
+});
 
-// Return different version of the data
-// depending on the state of the application.
-const reducer = (state, data) => {
-  const [dateMin, dateMax] = state.selected
-  return data.filter(d => {
-    return d.properties.date >= dateMin && d.properties.date <= dateMax
-  })
-}
+d3.selectAll( '#buttons button.zooming' ).on( 'click', function(){
+    var scale       =   1;
+    var direction   =   d3.select(this).attr("class").replace( 'zooming ', '' );
 
-const startup = async () => {
-  let state = {
-    selected: [new Date(), new Date()]
-  }
+    if( direction === "in" ){
+        scale       =  1.25;
+    }else if(direction === "out"){
+        scale       =  0.75;
+    }
 
-  const data = (await (
-    await window.fetch('public/geolocs.json')
-  ).json()).features.map(type)
-
-  state.selected = d3.extent(data, d => d.properties.date)
-
-  const toRender = render()
-  renderBrush((mapped) => {
-    state.selected = mapped
-    toRender(reducer(state, data))
-  })(data)
-
-  window.addEventListener('resize', event => {
-    toRender(reducer(state, data))
-  })
-
-  toRender(reducer(state, data))
-}
-
-startup()
+    map.transition()
+        .call(zoom_map.scaleBy, scale);
+});
